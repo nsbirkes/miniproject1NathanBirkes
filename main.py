cf58939ab698a5f4745e8755ee4ad6b3b525425f
nbirkes28 <nsbirkes@gmail.com>
2026-02-15 14:08:17 -0600

    Initial commit - creation of folders, imports, etc.

---
+++ main.py
@@ -0,0 +1,11 @@
+# INF601 - Advanced Programming in Python
+
+# Nathan Birkes
+
+# Mini Project 1
+
+import os
+import matplotlib as mp
+import numpy as np
+import yfinance as yf
+from datetime import datetime
